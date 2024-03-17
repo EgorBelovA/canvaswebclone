925530e1dce6010ce1f56682cf37fc12e7baf622
Egor Belov <ebelov54@gmail.com>
2024-03-17 11:33:15 +0300

    docker was updated

--- canvas/urls.py
+++ canvas/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.views.generic import TemplateView
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
 urlpatterns = [
